03d67c3471f4896fceaed14888ae6561e3bc6ee9
amilamilmuhammed <84724256+amilamilmuhammed@users.noreply.github.com>
2024-11-20 01:26:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,21 +6,21 @@ id_pattern = re.compile(r'^.\d+$')
 class Config(object):
 
     # Client Config 
-    API_ID = int(os.environ.get('API_ID', '')) # ⚠️ Required
-    API_HASH = os.environ.get('API_HASH', '') # ⚠️ Required
-    BOT_TOKEN = os.environ.get('BOT_TOKEN', '') # ⚠️ Required
+    API_ID = int(os.environ.get('API_ID',28892113 '')) # ⚠️ Required
+    API_HASH = os.environ.get('API_HASH', dc4cb3e9a4144280688ee20415529b15'') # ⚠️ Required
+    BOT_TOKEN = os.environ.get('BOT_TOKEN',7576309745:AAFaOpG6Z0o-cakFOe2aQoOjF3EiKanpblo '') # ⚠️ Required
 
     # database config
-    DB_URL  = os.environ.get("DB_URL","") # ⚠️ Required
+    DB_URL  = os.environ.get("DB_URL",mongodb+srv://amilmuhammed3:cHaUKJYudmXDICa4@cluster0.mbo75.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"") # ⚠️ Required
 
     # Other Config 
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "") # ⚠️ Required
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", @AttQwe_bot"") # ⚠️ Required
     BOT_UPTIME  = time.time()
-    OWNER = int(os.environ.get('OWNER', '')) # ⚠️ Required
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "")) # ⚠️ Required
+    OWNER = int(os.environ.get('OWNER', 7600777042'')) # ⚠️ Required
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1002338834649."")) # ⚠️ Required
     APPROVED_WELCOME_TEXT = os.environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYou're Auto Approved. ✅")
     LEAVING_BY_TEXT = os.environ.get("APPROVED_WELCOME_TEXT", "👋 Bye {mention} !\nSee You Soon by {title}\n\nYou Left. ⛔")
-    FORCE_SUB = os.environ.get('FORCE_SUB', '') # ⚠️ Required
+    FORCE_SUB = os.environ.get('FORCE_SUB', -1002338834649.'') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None 
 
